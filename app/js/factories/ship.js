f238ccd1659c1e4ae8f64401b5b94c9164b4be66
Warren Grant Russell <Link685@Warrens-MacBook-Pro.local>
2014-07-26 17:38:10 -0700

    twenty cells with boats

--- app/js/factories/ship.js
+++ app/js/factories/ship.js
@@ -1,28 +1,23 @@
-var App = angular.module('drag-and-drop', ['ngDragDrop']);
+// var App = angular.module('drag-and-drop', ['ngDragDrop']);
 
-App.controller('oneCtrl', function($scope, $timeout) {
-  $scope.board = [];
- 
+// App.controller('shit', function($scope, $timeout) {
+//   $scope.board = [];
+
   
-  $scope.list5 = [
-    { 'title': 'USS Z', 'drag': true },
-    { 'title': 'USS W', 'drag': true },
-    { 'title': 'USS S', 'drag': true },
-    { 'title': 'USS M', 'drag': true },
-    { 'title': 'USS V', 'drag': true },
-    { 'title': 'USS P', 'drag': true },
-    { 'title': 'USS T', 'drag': true },
-    { 'title': 'USS A', 'drag': true }
-  ];
+//   $scope.list5 = [
+//     { 'title': 'Item 1', 'drag': true },
+//     { 'title': 'Item 2', 'drag': true },
 
-  // Limit items to be dropped in list1
-  $scope.optionsBoard = {
-    accept: function(dragEl) {
-      if ($scope.board.length >= 20) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  };
-});
+//   ];
+
+//   // Limit items to be dropped in list1
+//   $scope.optionsBoard = {
+//     accept: function(dragEl) {
+//       if ($scope.board.length >= 20) {
+//         return false;
+//       } else {
+//         return true;
+//       }
+//     }
+//   };
+// });

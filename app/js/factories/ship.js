b123b04d513da879652fea5fec9d0f410e2a8628
WarrenGGrussell <warrencoke@gmail.com>
2014-07-29 11:03:29 -0700

    drop ev refactor

--- app/js/factories/ship.js
+++ app/js/factories/ship.js
@@ -2,7 +2,14 @@ app.factory('Ship', function () {
 
   'use strict';
 
-  var ship;
+  var ship = Ship.get();
+    $scope.ships = {
+    frigate: Ship.create("frigate", 3),
+    corvette: Ship.create("corvette", 2),
+    destroyer: Ship.create("destroyer", 4),
+    cruiser: Ship.create("cruiser", 5),
+    carrier: Ship.create("carrier", 6)
+  };
 
   return{
     create : function (name, length) {
